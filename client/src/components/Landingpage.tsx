aaf41a1969ff54a24debfb7e2ca758f8834e74c5
Nitin Budakoti <manuboda8885@gmail.com>
2025-04-06 18:02:41 +0530

    heroSection updated

--- client/src/components/Landingpage.tsx
+++ client/src/components/Landingpage.tsx
@@ -1,9 +1,19 @@
 import ImageSlider from "./imageSlider";
+import heroSection from "../assets/heroSection.jpg";
 const LandingPage = () => {
   return <div className="bg-green-500">
+      <HeroSection />
       <ImageSlider />
     </div>
 
 };
 export default LandingPage;
 
+const HeroSection = () => {
+  return <div
+  className="h-screen flex flex-col justify-center items-center text-black bg-cover bg-center"
+  style={{ backgroundImage: `url(${heroSection})` }}>
+    <h1 className="text-5xl font-bold mb-4">Welcome to Rishikesh</h1>
+    <h2 className="text-5xl font-bold"> Explore the uttrakhands best tourist place</h2> 
+    </div>;
+};
